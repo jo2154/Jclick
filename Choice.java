e2b165ea58c91dd3b74ecf13fa85fe2cb40cac4a
jo2154 <142521556+jo2154@users.noreply.github.com>
2025-07-19 19:11:51 +0530

    Add files via upload

---
+++ Choice.java
@@ -0,0 +1,22 @@
+package Selenium;
+
+public class Choice
+{
+	 public static void main(String[] args) {
+		 {
+			    
+			   int a=10;
+			   int b=20;
+			   int c=24;
+			   int d=25;
+			   
+			   
+			   
+			   
+			 
+		 }
+		   
+	
+}
+
+}
